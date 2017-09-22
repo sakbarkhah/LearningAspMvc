ef95f4e9269689d759f9726ee0582df7265d3a7a
sakbarkhah <s.akbarkhah@gmail.com>
2017-09-22 16:59:47 +0330

    Practise Redirect

---
+++ LearningAspMvc/LearningAspMvc/Controllers/PractiseRedirectController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningAspMvc.Controllers
+{
+    public class PractiseRedirectController : Controller
+    {
+        // GET: PractiseRedirect
+        public ActionResult Index()
+        {
+            if(Request.Cookies["LinkCookie"] == null)
+                Response.SetCookie(CreateLinkCookie());
+            return View();
+        }
+        public ActionResult RedirectTest()
+        {
+            if (Request.Cookies["LinkCookie"].Value == "0")
+                {
+                    Response.Cookies["LinkCookie"].Value = "1";
+                    return RedirectToAction("FakePage");
+                }
+                else if (Request.Cookies["LinkCookie"].Value == "1")
+                {
+                    Response.Cookies["LinkCookie"].Value = "0";
+                    return RedirectToAction("MainPage");
+                }
+           else
+                return View();               
+        }
+        [NonAction]
+        private HttpCookie CreateLinkCookie()
+        {
+            HttpCookie LinkCookie = new HttpCookie("LinkCookie", "0");
+            LinkCookie.Expires = DateTime.Now.AddMinutes(1);
+            return LinkCookie;
+        }
+        public string FakePage()
+        {
+            return "<center>This Is A Fake Page :D</center>";
+        }
+        public string MainPage()
+        {
+            return "<center>WOW! This is the main page</center>";
+        }
+    }
+}
